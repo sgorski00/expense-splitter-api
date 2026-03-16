7a9280c544ca7fdda66c69e1b111b08c16532546
sgorski00 <sebastiangorski00@gmail.com>
2026-03-16 14:27:07 +0100

    refactor: replace log4j2 with slf4j, add psql healthcheck

--- src/main/java/pl/sgorski/expense_splitter/web/GlobalExceptionHandler.java
+++ src/main/java/pl/sgorski/expense_splitter/web/GlobalExceptionHandler.java
@@ -1,13 +1,13 @@
 package pl.sgorski.expense_splitter.web;
 
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-@Log4j2
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
