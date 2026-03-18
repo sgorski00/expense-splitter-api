5f490c960e09d40a4fe1ab8beea76671b30fb602
sgorski00 <sebastiangorski00@gmail.com>
2026-03-18 08:45:24 +0100

    fix: add deny all in security config

--- src/main/java/pl/sgorski/expense_splitter/security/config/SecurityConfig.java
+++ src/main/java/pl/sgorski/expense_splitter/security/config/SecurityConfig.java
@@ -17,7 +17,8 @@ public class SecurityConfig {
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .requestMatchers("/auth/**").not().authenticated()
                         .requestMatchers("/expenses/**", "/friendships/**", "/payments/**", "/profile/**").authenticated()
-                        .requestMatchers("/users/**").hasRole("ADMIN"))
+                        .requestMatchers("/users/**").hasRole("ADMIN")
+                        .anyRequest().denyAll())
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
