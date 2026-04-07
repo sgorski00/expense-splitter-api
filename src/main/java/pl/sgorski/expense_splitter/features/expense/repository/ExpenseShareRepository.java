0db0fa045ecf49a545aa82f935be9cfebff2bdc9
sgorski00 <sebastiangorski00@gmail.com>
2026-04-07 13:14:51 +0200

    refactor: remove unused repository

--- src/main/java/pl/sgorski/expense_splitter/features/expense/repository/ExpenseShareRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.sgorski.expense_splitter.features.expense.repository;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.sgorski.expense_splitter.features.expense.domain.ExpenseShare;
-
-public interface ExpenseShareRepository extends JpaRepository<ExpenseShare, UUID> {}
