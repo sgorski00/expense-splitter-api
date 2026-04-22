6f2a5b890e1176f412e5e552d6c6a7954b96f63a
sgorski00 <sebastiangorski00@gmail.com>
2026-04-22 09:55:47 +0200

    change parent class of domain exception, remove event from jacoc exclusion

--- src/main/java/pl/sgorski/expense_splitter/exceptions/authentication/FailedLoginAttemptException.java
+++ src/main/java/pl/sgorski/expense_splitter/exceptions/authentication/FailedLoginAttemptException.java
@@ -1,6 +1,8 @@
 package pl.sgorski.expense_splitter.exceptions.authentication;
 
-public final class FailedLoginAttemptException extends RuntimeException {
+import org.springframework.security.core.AuthenticationException;
+
+public final class FailedLoginAttemptException extends AuthenticationException {
   public FailedLoginAttemptException(String message) {
     super(message);
   }
